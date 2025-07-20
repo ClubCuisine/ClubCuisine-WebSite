76217aa4b6036156caaf525d522d9f81f6c9d4bc
alienor basler <alienor.basler@gmail.com>
2025-07-20 15:48:12 +0200

    mise a jour site

--- README.md
+++
@@ -1,3 +0,0 @@
-# Site web du Club Cuisine ESIEE PARIS
-
-Bienvenue sur le git du site web du Club Cuisine de l'ESIEE Paris. Ce site a été créé pour partager notre passion pour la cuisine et fournir des informations sur nos recettes.
--- js/tags.js
+++ js/tags.js
@@ -1,6 +1,6 @@
-const tags = ["Italie", "Rapide", "Facile", "Four", "Sans alcool", "Sans porc"];
+const tags = ["Sans alcool", "Facile", "Four"];
 const tagIcons = {"france": "material/flag", "italie": "material/flag", "espagne": "material/flag", "mexique": "material/flag", "etats_unis": "material/flag", "bresil": "material/flag", "argentine": "material/flag", "perou": "material/flag", "chine": "material/flag", "japon": "material/flag", "coree_du_sud": "material/flag", "thailande": "material/flag", "vietnam": "material/flag", "inde": "material/flag", "maroc": "material/flag", "turquie": "material/flag", "grece": "material/flag", "liban": "material/flag", "egypte": "material/flag", "afrique_du_sud": "material/flag", "russie": "material/flag", "allemagne": "material/flag", "suisse": "material/flag", "belgique": "material/flag", "portugal": "material/flag", "australie": "material/flag", "nouvelle_zelande": "material/flag", "entree": "fontawesome/solid/utensils", "plat": "material/food", "dessert": "material/ice-cream", "vegetarien": "material/leaf", "vegetalien": "material/leaf", "sans_gluten": "material/bread-slice", "sans_lactose": "material/cow", "sans_fruits_a_coque": "material/leaf", "facile": "material/emoticon-happy", "moyen": "material/alert", "difficile": "material/emoticon-sad", "rapide": "material/clock", "long": "material/clock", "four": "material/toaster-oven", "sans_four": "material/toaster-oven", "sans_porc": "material/pig-variant", "sans_alcool": "material/glass-cocktail-off"};
-const recipes = [{"link": "/Recettes/Entr\u00e9es/Salade de tomate.html", "tags": ["Italie", "Facile", "Rapide", "Sans porc", "Sans alcool"], "title": "Salade de tomate"}, {"link": "/Recettes/Entr\u00e9es/Cake aux olives et jambon.html", "tags": ["Facile", "Sans alcool", "Four"], "title": "Cake aux olives et jambon"}];
+const recipes = [{"link": "/Recettes\\Mini pain burger.html", "tags": ["Facile", "Sans alcool", "Four"], "title": "Mini pain burger"}, {"link": "/Recettes\\Steak Burger.html", "tags": ["Facile", "Sans alcool"], "title": "Steak Burger"}, {"link": "/Recettes\\Entr\u00e9es\\Cake aux olives et jambon.html", "tags": ["Facile", "Sans alcool", "Four"], "title": "Cake aux olives et jambon"}, {"link": "/Recettes\\Entr\u00e9es\\Mini burger.html", "tags": ["Facile", "Sans alcool"], "title": "Mini burger"}];
 
     document.addEventListener('DOMContentLoaded', function() {
         const tagInput = document.getElementById('tag-search');
@@ -10,10 +10,10 @@ const recipes = [{"link": "/Recettes/Entr\u00e9es/Salade de tomate.html", "tags"
         const selectedTags = new Set(JSON.parse(localStorage.getItem('selectedTags')) || []);
         let currentQuery = '';
 
-        // Afficher tous les tags par défaut avec icônes
+        // Afficher tous les tags par d�faut avec ic�nes
         function renderTags() {
             const filteredTags = tags.filter(tag => tag.toLowerCase().includes(currentQuery));
-            const sortedTags = filteredTags.sort((a, b) => a.localeCompare(b)); // Trier les tags par ordre alphabétique
+            const sortedTags = filteredTags.sort((a, b) => a.localeCompare(b)); // Trier les tags par ordre alphab�tique
             tagList.innerHTML = sortedTags.map(tag => {
                 const isSelected = selectedTags.has(tag);
                 return `<li class="md-tag md-tag-icon md-tag--${tag.toLowerCase().replace(/ /g, '_')} ${isSelected ? 'selected' : ''}">${tag}</li>`;
@@ -32,19 +32,19 @@ const recipes = [{"link": "/Recettes/Entr\u00e9es/Salade de tomate.html", "tags"
                 return Array.from(selectedTags).every(tag => recipeTags.includes(tag));
             });
 
-            const sortedRecipes = filteredRecipes.sort((a, b) => a.title.localeCompare(b.title)); // Trier les résultats par ordre alphabétique
+            const sortedRecipes = filteredRecipes.sort((a, b) => a.title.localeCompare(b.title)); // Trier les r�sultats par ordre alphab�tique
             resultList.innerHTML = `<h3 class="result-list-item">Résultats de filtrage :</h3>` + sortedRecipes.map(recipe => `<li class="result-list-item"><a href="${recipe.link}">${recipe.title}</a></li>`).join('');
             resultList.style.display = 'block';
 
-            // Ajouter la classe 'show' avec un léger délai pour l'animation
+            // Ajouter la classe 'show' avec un l�ger d�lai pour l'animation
             setTimeout(() => {
                 document.querySelectorAll('.result-list-item').forEach(item => {
                     item.classList.add('show');
                 });
-            }, 10); // Délai de 10ms pour permettre le rendu initial
+            }, 10); // D�lai de 10ms pour permettre le rendu initial
         }
 
-        // Initialiser l'affichage des tags et des résultats
+        // Initialiser l'affichage des tags et des r�sultats
         renderTags();
         updateResults();
 
@@ -77,7 +77,7 @@ const recipes = [{"link": "/Recettes/Entr\u00e9es/Salade de tomate.html", "tags"
             }
         });
 
-        // Restaurer les tags sélectionnés à partir du localStorage
+        // Restaurer les tags s�lectionn�s � partir du localStorage
         if (selectedTags.size > 0) {
             renderTags();
             updateResults();
